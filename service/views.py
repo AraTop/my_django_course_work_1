88dd4dfa445b1d1ad66c3449893145747e1fbb88
Grigory <lololohka057@gmail.com>
2023-08-05 21:40:53 +0300

    test_6_1

--- service/views.py
+++ service/views.py
@@ -1,11 +1,9 @@
-from datetime import datetime, timedelta
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from service.models import Customer_Service, Settings, Message_to_Send, Mailing_Logs 
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
@@ -22,17 +20,21 @@ class CustomerCreateView(CreateView):
       user.set_password(form.cleaned_data['password'])
       user.save()
       return super().form_valid(form)
-   
+
+@method_decorator(login_required, name='dispatch')
 class CustomerDeleteView(DeleteView):
    model = Customer_Service
    success_url = '/service' 
 
+@method_decorator(login_required, name='dispatch')
 class CustomerListView(ListView):
    model = Customer_Service
 
+@method_decorator(login_required, name='dispatch')
 class CustomerDetailView(DetailView):
    model = Customer_Service
    
+@method_decorator(login_required, name='dispatch')
 class CustomerUpdateView(UpdateView):
    model = Customer_Service
    fields = ('email', 'last_name', 'first_name', 'surname', 'comment', 'password')
@@ -48,7 +50,7 @@ class CustomerUpdateView(UpdateView):
       return reverse('service:service', args=[self.kwargs.get('pk')])
    
 #---------------------------------------------------------------------------
-
+@method_decorator(login_required, name='dispatch')
 class SettingsCreateView(LoginRequiredMixin, CreateView):
    model = Settings
    fields = ('mailing_time', 'periodicity', 'mailing_status', 'client')
@@ -60,16 +62,20 @@ class SettingsCreateView(LoginRequiredMixin, CreateView):
       context['objects'] = settings_objects
       return context
 
+@method_decorator(login_required, name='dispatch')
 class SettingsDeleteView(DeleteView):
    model = Settings
    success_url = '/service/settings/' 
 
+@method_decorator(login_required, name='dispatch')
 class SettingsListView(ListView):
    model = Settings
 
+@method_decorator(login_required, name='dispatch')
 class SettingsDetailView(DetailView):
    model = Settings
-   
+
+@method_decorator(login_required, name='dispatch')   
 class SettingsUpdateView(UpdateView):
    model = Settings
    fields = ('mailing_time', 'periodicity', 'mailing_status', 'client')
@@ -82,7 +88,7 @@ class SettingsUpdateView(UpdateView):
       return context
    
 #--------------------------------------------------------------
-
+@method_decorator(login_required, name='dispatch')
 class Message_to_SendCreateView(CreateView):
    model = Message_to_Send
    fields = ('letter_subject', 'letter_body', 'settings')
@@ -94,16 +100,20 @@ class Message_to_SendCreateView(CreateView):
       context['objects'] = settings_objects
       return context
 
+@method_decorator(login_required, name='dispatch')
 class Message_to_SendDeleteView(DeleteView):
    model = Message_to_Send
    success_url = '/service/message/' 
 
+@method_decorator(login_required, name='dispatch')
 class Message_to_SendListView(ListView):
    model = Message_to_Send
 
+@method_decorator(login_required, name='dispatch')
 class Message_to_SendDetailView(DetailView):
    model = Message_to_Send
    
+@method_decorator(login_required, name='dispatch')
 class Message_to_SendUpdateView(UpdateView):
    model = Message_to_Send
    fields = ('letter_subject', 'letter_body', 'settings')
@@ -127,17 +137,21 @@ class Mailing_LogsCreateView(CreateView):
       settings_objects = Settings.objects.all()
       context['objects'] = settings_objects
       return context
-
+   
+@method_decorator(login_required, name='dispatch')
 class Mailing_LogsDeleteView(DeleteView):
    model = Mailing_Logs
    success_url = '/service/logs/' 
 
+@method_decorator(login_required, name='dispatch')
 class Mailing_LogsListView(ListView):
    model = Mailing_Logs
 
+@method_decorator(login_required, name='dispatch')
 class Mailing_LogsDetailView(DetailView):
    model = Mailing_Logs
    
+@method_decorator(login_required, name='dispatch')
 class Mailing_LogsUpdateView(UpdateView):
    model = Mailing_Logs
    fields = ('date_and_time_of_last_attempt', 'attempt_status', 'mail_server_response', 'settings')
